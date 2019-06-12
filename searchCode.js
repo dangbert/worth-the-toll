2326528eeb5cfcb6e530450f1a2462be47e49619
Daniel Engbert <end1@umbc.edu>
2019-06-11 23:24:24 -0400

    Everything works for simple 1 way time difference display. No UI for creating a URL yet.

--- searchCode.js
+++ searchCode.js
@@ -1,11 +1,15 @@
-// https://developers.google.com/maps/documentation/javascript/reference?hl=en
-const homeId = "ChIJ2V1MbzTdt4kRYN9Qf8r1y38"; // place_id
-const workId = "ChIJ651lxsIrtokRDijRKFtvbB4";
+/**
+ * setup page to help user create a valid url
+ * (only call after google maps api has loaded)
+ *
+ * errMsg: (optional) message to display in alert before setting up page
+ */
+function setup(errMsg) {
+    if (errMsg !== undefined)
+        alert(errMsg);
+    window.history.pushState({}, document.title, "/");
 
-// called after the google maps api is loaded
-function initMaps() {
     var searchBox = new google.maps.places.SearchBox(document.getElementById('mysearchbox'));
-
     //if searchbox is used
     searchBox.addListener('places_changed', function () {
         //document.getElementById("mysearchbox").value = ""; //clear searchbox
@@ -22,31 +26,69 @@ function initMaps() {
         
         //document.getElementById("startInfo").innerHTML = "<br>" + start['formatted_address'];
     });
+}
 
 
+/**
+ * function called after the google maps api is loaded
+ */
+function initMaps() {
+    var homeId = String(getUrlParams()["home"]);
+    var workId = String(getUrlParams()["work"]);
+    if (homeId == undefined || workId == undefined) {
+        setup();
+        return;
+    }
+    console.log("'" + homeId + "'");
+    console.log("'" + workId + "'");
+
+
+    // calculate time difference
     getLocById(homeId, function(homeLoc) {
         getLocById(workId, function(workLoc) {
             console.log(homeLoc);
             console.log(workLoc);
 
-            // now get
-            getDuration(homeLoc, workLoc, true, function(durTolls) {
-              getDuration(homeLoc, workLoc, false, function(durNoTolls) {
+            // now get duration of each route
+            getDuration(homeLoc.geometry.location, workLoc.geometry.location, true, function(durTolls) {
+                getDuration(homeLoc.geometry.location, workLoc.geometry.location, false, function(durNoTolls) {
+                    console.log(durTolls);
+                    console.log(durNoTolls);
+                    // calculate time difference
+                    var diffSec = durNoTolls.value - durTolls.value;
+                    var diffMin = Math.floor(diffSec / 60);
+                    var diffHour = Math.floor(diffMin / 60);
+                    diffMin = diffMin - diffHour * 60;
+                    diffSec = diffSec - diffMin * 60;
 
-              });
+                    var diffString = "";
+                    if (diffHour !== 0) {
+                        diffString += diffHour + "h ";
+                    }
+                    diffString += diffMin + "m";
+                    console.log(diffString);
+
+                    // display results
+                    // TODO: also display total without tolls
+                    var res = "(" + diffString + " extra avoiding tolls)";
+                    console.log(res);
+
+                    $("#home-address").html(homeLoc.formatted_address);
+                    $("#work-address").html(workLoc.formatted_address);
+                    $("#result-info").html(res);
+
+                });
             });
-
         });
     });
-
-
 }
 
 /**
  * find the duration of a driving route
+ *
  * loc1: latlng object
  * loc2: latlng object
- * allowTolls: bool true if tolls enabled
+ * allowTolls: bool (true if tolls enabled)
  * callback: function to pass the resulting duration object
 */
 function getDuration(loc1, loc2, allowTolls, callback) {
@@ -73,23 +115,40 @@ function getDuration(loc1, loc2, allowTolls, callback) {
             }
             console.log("time: " + best.text);
             callback(best);
-        }
+        } else
+            setup("Error: calculating direction information failed");
     });
-
 }
 
-// place_id lookup: https://developers.google.com/maps/documentation/javascript/examples/geocoding-place-id
+/**
+ * lookup a location by placeId
+ *
+ * callback: function to pass the result object
+ *           (its .geometry.location property is a latlng object)
+ *
+ * https://developers.google.com/maps/documentation/javascript/examples/geocoding-place-id
+ */
 function getLocById(placeId, callback) {
     var geocoder = new google.maps.Geocoder;
     geocoder.geocode({'placeId': placeId}, function(results, status) {
         if (status === 'OK') {
             if (results[0]) {
-                //console.log(results[0]);
-                callback(results[0].geometry.location);
-            }
-            else
-                window.alert('No results found');
+                ///callback(results[0].geometry.location);
+                callback(results[0]);
+            } else
+                setup("Error: Geocoder unable to find results for url parameter");
         } else
-        window.alert('Geocoder failed due to: ' + status);
+            setup("Error: Geocoder failed due to: " + status);
     });
+}
+
+/**
+ * returns dict of URL parameters
+ */
+function getUrlParams() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
 }
